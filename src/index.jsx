56d1bff1fe4e5a582232771bd3c8387c731c148b
Velenir <Velenir@users.noreply.github.com>
2017-01-26 19:18:04 +0300

    Use initialState instead of action on startup

--- src/index.jsx
+++ src/index.jsx
@@ -5,22 +5,21 @@ import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducer';
 import {TodoAppContainer} from './components/TodoApp';
+import {fromJS} from 'immutable';
 
 import 'todomvc-app-css/index.css';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    todos: [
-      {id: 1, text: 'React', status: 'active', editing: false},
-      {id: 2, text: 'Redux', status: 'active', editing: false},
-      {id: 3, text: 'Immutable', status: 'active', editing: false},
-    ],
-    filter: 'all'
-  }
+const initialState = fromJS({
+  todos: [
+    {id: 1, text: 'React', status: 'active', editing: false},
+    {id: 2, text: 'Redux', status: 'active', editing: false},
+    {id: 3, text: 'Immutable', status: 'active', editing: false},
+  ],
+  filter: 'all'
 });
+
+const store = createStore(reducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 
 ReactDOM.render(
   <Provider store={store}>
